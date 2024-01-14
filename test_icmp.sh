9e9698534d8f8ca2ebd11fa4e2af850e8d94311d
Arian Rezazadeh <notarian.rez@gmail.com>
2024-01-14 16:48:49 +0330

    revamp integration tests

--- test_icmp.sh
+++ test_icmp.sh
@@ -1,3 +1,3 @@
-bin_name=$(cargo t --no-run 2>&1 | grep -oP 'target/debug/deps/\S+(?=\))')
-sudo setcap cap_net_admin,cap_net_raw=eip $bin_name
+bin_name=$(cargo t --no-run 2>&1 | grep -oP 'target/debug/deps/integration_test\S+(?=\))')
+sudo setcap cap_net_admin,cap_net_raw=eip target/debug/forwarder
 ./$bin_name --nocapture --color always --ignored icmp
