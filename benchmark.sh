3102aaa8b0a59520b1527507cf547f57417b5d35
Arian Rezazadeh <notarian.rez@gmail.com>
2024-01-21 12:22:56 +0330

    disable linux automatic icmp echo reply when benchmarking

--- benchmark.sh
+++ benchmark.sh
@@ -8,7 +8,12 @@ cargo b --release
 bin_name=./target/release/forwarder
 
 if [ "$PROTOCOL" = "icmp" ]; then
+    # icmp uses raw socket and raw socket needs permission
     sudo setcap cap_net_admin,cap_net_raw=eip $bin_name
+
+    # disable automatic icmp reply messages for better and accurate performance
+    DEFAULT_ICMP_IGNORE=$(cat /proc/sys/net/ipv4/icmp_echo_ignore_all)
+    sudo sh -c "echo 1 > /proc/sys/net/ipv4/icmp_echo_ignore_all"
 fi
 
 run_forwarder() {
@@ -25,4 +30,10 @@ iperf_server=$!
 sleep 1
 iperf -c 127.0.0.1 -p 3536 -u -b 1G -i 1 -e
 
-trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+on_close() {
+    if [ "$PROTOCOL" = "icmp" ]; then
+        sudo sh -c "echo $DEFAULT_ICMP_IGNORE > /proc/sys/net/ipv4/icmp_echo_ignore_all"
+    fi
+    kill -9 -$$
+}
+trap on_close SIGINT SIGTERM EXIT
