bdcd3bea01c888683b65dbca540a889ffddc621d
Arian Rezazadeh <notarian.rez@gmail.com>
2024-09-15 14:43:46 +0330

    more readable error for socket

--- lib/src/socket/mod.rs
+++ lib/src/socket/mod.rs
@@ -1,15 +1,15 @@
 use enum_dispatch::enum_dispatch;
-use std::{io::Result, net::SocketAddr};
+use std::{io, net::SocketAddr};
 
 #[enum_dispatch]
 pub trait SocketTrait {
-    fn recv(&self, buffer: &mut [u8]) -> Result<usize>;
-    fn recv_from(&self, buffer: &mut [u8]) -> Result<(usize, SocketAddr)>;
-    fn send_to(&self, buffer: &[u8], to: &SocketAddr) -> Result<usize>;
-    fn send(&self, buffer: &[u8]) -> Result<usize>;
-    fn connect(&self, addr: &SocketAddr) -> Result<()>;
-    fn local_addr(&self) -> Result<SocketAddr>;
-    fn set_nonblocking(&self, nonblocking: bool) -> Result<()>;
+    fn recv(&self, buffer: &mut [u8]) -> io::Result<usize>;
+    fn recv_from(&self, buffer: &mut [u8]) -> io::Result<(usize, SocketAddr)>;
+    fn send_to(&self, buffer: &[u8], to: &SocketAddr) -> io::Result<usize>;
+    fn send(&self, buffer: &[u8]) -> io::Result<usize>;
+    fn connect(&self, addr: &SocketAddr) -> io::Result<()>;
+    fn local_addr(&self) -> io::Result<SocketAddr>;
+    fn set_nonblocking(&self, nonblocking: bool) -> io::Result<()>;
     fn as_raw_fd(&self) -> i32;
 }
 
--- lib/src/socket/udp.rs
+++ lib/src/socket/udp.rs
@@ -1,42 +1,42 @@
 use super::SocketTrait;
-use std::{io::Result, net::SocketAddr, os::fd::AsRawFd};
+use std::{io, net::SocketAddr, os::fd::AsRawFd};
 
 #[derive(Debug)]
 pub struct UdpSocket(std::net::UdpSocket);
 
 impl UdpSocket {
-    pub fn bind(address: &SocketAddr) -> Result<Self> {
+    pub fn bind(address: &SocketAddr) -> io::Result<Self> {
         let socket = std::net::UdpSocket::bind(address)?;
         Ok(UdpSocket(socket))
     }
 }
 
 impl SocketTrait for UdpSocket {
-    fn recv(&self, buffer: &mut [u8]) -> Result<usize> {
+    fn recv(&self, buffer: &mut [u8]) -> io::Result<usize> {
         self.0.recv(buffer)
     }
 
-    fn recv_from(&self, buffer: &mut [u8]) -> Result<(usize, SocketAddr)> {
+    fn recv_from(&self, buffer: &mut [u8]) -> io::Result<(usize, SocketAddr)> {
         self.0.recv_from(buffer)
     }
 
-    fn send_to(&self, buffer: &[u8], to: &SocketAddr) -> Result<usize> {
+    fn send_to(&self, buffer: &[u8], to: &SocketAddr) -> io::Result<usize> {
         self.0.send_to(buffer, to)
     }
 
-    fn send(&self, buffer: &[u8]) -> Result<usize> {
+    fn send(&self, buffer: &[u8]) -> io::Result<usize> {
         self.0.send(buffer)
     }
 
-    fn connect(&self, addr: &SocketAddr) -> Result<()> {
+    fn connect(&self, addr: &SocketAddr) -> io::Result<()> {
         self.0.connect(addr)
     }
 
-    fn local_addr(&self) -> Result<SocketAddr> {
+    fn local_addr(&self) -> io::Result<SocketAddr> {
         self.0.local_addr()
     }
 
-    fn set_nonblocking(&self, nonblocking: bool) -> Result<()> {
+    fn set_nonblocking(&self, nonblocking: bool) -> io::Result<()> {
         self.0.set_nonblocking(nonblocking)
     }
 
